4c36a9fc2115eb0796bed5045ec64d1e2167abee
milan090 <milanmuhammed1@gmail.com>
2021-01-10 08:10:09 +0000

    handle mobile users

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,14 @@ import "../styles/tailwind.css";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <div className="sm:hidden">
+        <h1 className="text-5xl font-bold mt-32 ml-5">Sorry <br/> We Do Not <br/> Support <br/> Mobile Yet</h1>
+      </div>
+      <div className="hidden sm:block">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
