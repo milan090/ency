d942ed95c8de915f3a36b1f0daa7224d431a7bad
milan090 <milanmuhammed1@gmail.com>
2021-01-10 20:06:05 +0530

    sign in page done

--- components/custom-button/custom-button.component.tsx
+++ components/custom-button/custom-button.component.tsx
@@ -3,26 +3,38 @@ import Link from "next/link";
 
 import { overrideTailwindClasses } from "tailwind-override";
 
-type Props = {
-  href?: string;
+interface CustomButtonBodyProps {
   className?: string;
   children: React.ReactNode;
+  color?: string;
+  bgColor?: string;
+  transitionColor?: string;
+  transitionBgColor?: string;
+  borderColor?: string;
 };
 
-type CustomButtonBodyProps = {
-  className?: string;
-  children: React.ReactNode;
+interface Props extends CustomButtonBodyProps {
+  href?: string;
 };
+
 
 const CustomButtonBody: React.FC<CustomButtonBodyProps> = ({
   className,
   children,
+  color,
+  bgColor,
+  transitionColor,
+  transitionBgColor,
+  borderColor
 }) => {
   return (
     <button
       className={overrideTailwindClasses(
-        `bg-primary rounded-lg text-white font-semibold px-4 py-1.5
-        transition-colors duration-300 ease-out focus:outline-none ${className}`
+        `bg-${bgColor || "primary"} rounded-lg text-${color || "white"} font-semibold px-4 py-1.5
+        border-2 border-solid border-${borderColor || "primary"}
+        transition-colors duration-300 ease-out focus:outline-none 
+        hover:text-${transitionColor || "primary"} hover:bg-${transitionBgColor || "white"}
+        ${className}`
       )}
     >
       <span>{children}</span>
@@ -30,16 +42,18 @@ const CustomButtonBody: React.FC<CustomButtonBodyProps> = ({
   );
 };
 
-const CustomButton: React.FC<Props> = ({ className, children, href }) => {
+const CustomButton: React.FC<Props> = ({ href, ...props }) => {
   if (href) {
     return (
       <Link href={href}>
-        <CustomButtonBody children={children} className={className} />
+        <a>
+          <CustomButtonBody {...props} />
+        </a>
       </Link>
     );
   }
 
-  return <CustomButtonBody children={children} className={className} />;
+  return <CustomButtonBody {...props} />;
 };
 
 export default CustomButton;
