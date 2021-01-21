86bcd510f82ae8ee5a4453ec6cde59646e1d07be
milan090 <milanmuhammed1@gmail.com>
2021-01-21 17:52:21 +0530

    testing fix

--- config/firebase-admin.ts
+++ config/firebase-admin.ts
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
       projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
     }),
