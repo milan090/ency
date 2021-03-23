75ec1539468fc10b1524ab0304189a7356a8015c
AineasGamingYolo <aineas.vasiliou@gmail.com>
2021-03-23 13:18:36 +0200

    e2e app fix

--- ency-backend/test/app.e2e-spec.ts
+++ ency-backend/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import Request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -16,9 +16,8 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return Request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(404)
   });
 });
