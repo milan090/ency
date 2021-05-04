976e4d79f27ca912625b8bdc351db7db450e66a8
milan090 <milanmuhammed1@gmail.com>
2021-05-04 07:55:16 +0530

    fix tailwind new config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.tsx", "./components/**/*.tsx", "./layouts/**/*.tsx"],
+  purge: ["./src/pages/**/*.tsx", "./src/client/components/**/*.tsx", "./src/client/layouts/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
