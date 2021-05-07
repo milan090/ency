87de7e0564f85ebd0e581b448c4e86f3938bbe8e
milan090 <milanmuhammed1@gmail.com>
2021-05-07 23:20:27 +0530

    feature: meta og tags for project page

--- src/pages/project/[id].tsx
+++ src/pages/project/[id].tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 
 import React from "react";
 import { graphqlClient, ssrCache } from "src/client/graphql/client";
@@ -8,20 +9,34 @@ import {
   GetProjectQueryVariables,
   useGetProjectQuery,
 } from "src/client/graphql/getProject.generated";
+import { getURL } from "src/server/getURL";
 
 export default function ProjectPage({ projectId }: { projectId: string }): React.ReactNode {
   console.log("ProjectPage");
   const [result] = useGetProjectQuery({ variables: { id: projectId } });
   const { data, fetching, error } = result;
 
-  if (fetching) return "Loading";
-  if (error) return "Error, oops";
+  if (!data?.project) {
+    if (fetching) return "Loading";
+    return "Error, oops";
+  }
+
+  const { title, description, color, iconSrc } = data.project;
 
   return (
-    <div>
-      <h1>{data?.project?.title}</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+
+        <meta property="og:title" content={`Ency | ${title}`} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={iconSrc || "/images/tmp/noto_rocket.png"} />
+        <meta property="og:url" content={`${getURL()}/`} />
+        <meta content={color} data-react-helmet="true" name="theme-color"></meta>
+        <title>{data.project.title}</title>
+      </Head>
+    </>
   );
 }
 
