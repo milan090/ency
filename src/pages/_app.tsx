5c93b21fbeb5f9b316420bb0dae5932d0f611387
milan090 <milanmuhammed1@gmail.com>
2021-05-07 23:26:32 +0530

    remove head tag in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
-import Head from "next/head";
 import { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "next-auth/client";
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import { TOAST_OPTIONS } from "src/config/toaster";
-import { getURL } from "src/server/getURL";
 import { GRAPHQL_ENDPOINT } from "src/client/graphql/client";
 
 import "src/client/styles/globals.css";
@@ -16,21 +14,6 @@ const App = ({ Component, pageProps }: AppProps): React.ReactNode => {
   const queryClient = new QueryClient();
   return (
     <Provider session={pageProps.session}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-
-        <meta
-          property="og:title"
-          content="Ency | Traditional Writing is good. But it could be better 👍"
-        />
-        <meta
-          property="og:description"
-          content="Presenting to you Ency, an AI Assisstant that helps you complete your reports, assignments and articles much faster & easier. There’s no turning back now!"
-        />
-        <meta property="og:image" content="/images/logo.png" />
-        <meta property="og:url" content={getURL()} />
-      </Head>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
         <Toaster position="bottom-right" toastOptions={TOAST_OPTIONS} />
